42981ee46acaf88c1218a76d86cdc0c2d99806d8
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-29 00:34:05 -0500

    header complete

--- PortFolioMinimalist/src/components/Socials.jsx
+++ PortFolioMinimalist/src/components/Socials.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import { social } from "../data";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <ul className="flex space-x-6">
+      {social.map((item, index) => {
+        return (
+          <li
+            className="flex justify-center items-center text-accent "
+            key={index}
+          >
+            <a href={item.href} className="text-base ">
+              {item.icon}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Socials;
