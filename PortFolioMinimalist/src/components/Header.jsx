d8e394d2f8ed9f608d8dfcb6c1a846c36ba4fbc7
Wilson Lluilema <57785568+lODIN007l@users.noreply.github.com>
2022-08-29 00:23:12 -0500

    hearo and header

--- PortFolioMinimalist/src/components/Header.jsx
+++ PortFolioMinimalist/src/components/Header.jsx
@@ -6,10 +6,38 @@ import NavMobile from "./NavMobile";
 import Socials from "./Socials";
 
 const Header = () => {
-  const [bg, setBg] = useState("");
+  const [bg, setBg] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      return window.scrollY > 50 ? setBg(true) : setBg(false);
+    });
+  }, []);
+
   return (
-    <header>
-      <div className="container mx-auto">Header</div>
+    <header
+      className={`${
+        bg ? "bg-tertiary h-20" : "h-24"
+      } flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300 `}
+    >
+      <div className="container mx-auto h-full flex items-center justify-between  ">
+        {/* logo */}
+        <a href="">
+          <img src={Logo} alt="" />
+        </a>
+        {/* nav */}
+        <div className="hidden lg:block">
+          <Nav />
+        </div>
+        {/* socials */}
+        <div className="hidden lg:block">
+          <Socials />
+        </div>
+        {/* nav mobile  */}
+        <div className=" lg:hidden">
+          <NavMobile />
+        </div>
+      </div>
     </header>
   );
 };
