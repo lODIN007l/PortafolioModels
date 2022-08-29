474556bb56159a648184a28d1d8639884bd43518
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-08-29 04:05:11 -0500

    brands create

--- PortFolioMinimalist/src/App.jsx
+++ PortFolioMinimalist/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Brands from "./components/Brands";
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Brands />
       <div style={{ height: "2000px" }}></div>
     </>
   );
