f3ba97db0609c8b034898f4285e7e4abadd35374
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-04 17:47:33 -0500

    inicio de projects

--- PortFolioMinimalist/src/App.jsx
+++ PortFolioMinimalist/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Brands from "./components/Brands";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Brands />
       <About />
       <Skills />
+      <Portfolio />
       <div style={{ height: "2000px" }}></div>
     </>
   );
---
+++ PortFolioMinimalist/src/components/Portfolio.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Projects from "./Projects";
+
+const Portfolio = () => {
+  return (
+    <section id="portfolio" className="section bg-primary min-h-[1400px] ">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h2>My latest work </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
