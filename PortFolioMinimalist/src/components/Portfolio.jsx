aaa0f5f02d3cc195a4d4781348faf3ec396ef10d
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-04 18:28:33 -0500

    project bejore title

--- PortFolioMinimalist/src/components/Portfolio.jsx
+++ PortFolioMinimalist/src/components/Portfolio.jsx
@@ -7,7 +7,9 @@ const Portfolio = () => {
     <section id="portfolio" className="section bg-primary min-h-[1400px] ">
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-center">
-          <h2>My latest work </h2>
+          <h2 className="section-title before:content-portfolio relative before:opacity-40 before:-top-[2rem] before:-left-3/4 before:hidden before:lg:block ">
+            My latest work{" "}
+          </h2>
         </div>
       </div>
     </section>
