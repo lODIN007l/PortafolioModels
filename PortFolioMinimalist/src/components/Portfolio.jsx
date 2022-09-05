3ec998049f84227ce89a83bd1e790b994116b424
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-09-05 13:45:13 -0500

    section projects

--- PortFolioMinimalist/src/components/Portfolio.jsx
+++ PortFolioMinimalist/src/components/Portfolio.jsx
@@ -8,9 +8,15 @@ const Portfolio = () => {
       <div className="container mx-auto">
         <div className="flex flex-col items-center justify-center">
           <h2 className="section-title before:content-portfolio relative before:opacity-40 before:-top-[2rem] before:-left-3/4 before:hidden before:lg:block ">
-            My latest work{" "}
+            My latest work
           </h2>
+          <p className="subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+            unde enim quia, itaque recusandae esse adipisci odio omnis modi
+            facilis nobis error ipsum sequi odit eos et fugiat dolorem. Nobis.
+          </p>
         </div>
+        <Projects />
       </div>
     </section>
   );
--- PortFolioMinimalist/src/components/Projects.jsx
+++ PortFolioMinimalist/src/components/Projects.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+//data
+import { projectsData } from "../data";
+import { projectsNav } from "../data";
+//componente
+import Project from "./Project";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const [item, setItem] = useState({ name: "all" });
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if (item.name === "all") {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category.toLowerCase() == item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (e, index) => {
+    setItem({ name: e.target.textContent.toLowerCase() });
+    setActive(index);
+  };
+
+  return (
+    <div>
+      {/* nav */}
+      <nav className="mb-12 max-w-xl mx-auto ">
+        <ul className="flex flex-col md:flex-row justify-evenly items-center text-white  ">
+          {projectsNav.map((item, index) => {
+            return (
+              <li
+                className={`${
+                  active === index ? "active" : ""
+                } cursor-pointer capitalize m-4`}
+                onClick={(e) => handleClick(e, index)}
+                key={index}
+              >
+                {item.name}
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      {/* projectos  */}
+      <section className="grid lg:grid-cols-3 gap-y-12 lg:gap-x-8 lg:gap-y-8">
+        {projects.map((item) => {
+          return <Project item={item} key={item.id} />;
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default Projects;
